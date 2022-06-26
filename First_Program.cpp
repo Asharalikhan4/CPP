41c7e47851c8f4ac3e0af017e7dfa028eaaa2d27
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-06-26 13:08:33 +0530

    First program file added

---
+++ First_Program.cpp
@@ -0,0 +1,19 @@
+// Double backslash for single line comment.
+/* this is used for 
+multiline comment*/
+
+// C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream> // input output library
+// using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+	// cout is used to print the string statement.
+	std::cout << "Hello World";
+
+	return 0; // used to returning the value from a function to tell function is finished.
+}
+
