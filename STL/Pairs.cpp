8fc93161c0bb85ebce6ed00877a2020bd3e632e0
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-06 20:05:50 +0530

    Pairs Added

---
+++ STL/Pairs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Pairs is a class in c++ which is used to store two values.
+    pair<int,string>p;
+    // Now for entering the value.
+    p = make_pair(2,"abc");
+    // Another way of initializing.
+    p = {2,"std"};
+    // We can aslo pass any pair to another by refrence or in a normal way.
+    pair<int,string> &p1 = p;  // here we are passing it by refrence. now we can aslo manipulate the data.
+    cout<<p.first<<" "<<p.second<<endl;
+
+    // we can aslo declare the pair of an array
+    int a[] = {1,2,3};
+    int b[] = {2,3,4};
+    pair<int,int> p_array[3];
+    p_array[0] = {1,2};
+    p_array[1] = {2,3};
+    p_array[2] = {3,4};
+
+    Swap(p_array[0], p_array[2]);  // When we do this then the whole pair will be swapped.
+
+    for(int i = 0; i < 3; ++i){
+        cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+    }
+    return 0;
+
+    
+}
